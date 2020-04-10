4d16c725fc6adf31113ff0eed7889e196f0ace53
Leah <sc14lps@leeds.ac.uk>
2020-04-10 13:40:31 +0100

    Added vareload command that reloads the config file

--- src/main/java/com/shields/VillagerAlerts.java
+++ src/main/java/com/shields/VillagerAlerts.java
@@ -1,5 +1,8 @@
 package com.shields;
 
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -26,26 +29,42 @@ public class VillagerAlerts extends JavaPlugin implements Listener {
     public void onDisable() {
     }
 
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("vareload")) {
+            this.reloadConfig();
+            config = this.getConfig();
+            sender.sendMessage(ChatColor.RED + "Villager Alerts has been reloaded");
+            return true;
+        }
+        return false;
+    }
+
+
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
         if (event.getEntity() instanceof Villager) {
             Villager villager = (Villager) event.getEntity();
             if (villager.getHealth() - event.getFinalDamage() < 0) {
-                sendMessageToPlayersWithinArea(villager.getProfession() + " was killed by " +
+                sendMessageToPlayersWithinConfiguredArea(getNameOfEntity(villager) + " was killed by " +
                         getNameOfEntity(event.getDamager()), villager);
             }
         }
     }
 
     private String getNameOfEntity(Entity entity) {
+        String name = entity.getType().toString();
+
         if (entity instanceof Player) {
-            return ((Player) entity).getDisplayName();
-        } else {
-            return entity.getType().toString();
+            name = ((Player) entity).getDisplayName();
         }
+        if (entity instanceof Villager && !((Villager) entity).getProfession().equals(Villager.Profession.NONE)) {
+            name = ((Villager) entity).getProfession().toString();
+        }
+        return name;
     }
 
-    private void sendMessageToPlayersWithinArea(String message, Villager villager) {
+    private void sendMessageToPlayersWithinConfiguredArea(String message, Villager villager) {
         double radius = config.getDouble("radius");
         for (Player player : getServer().getOnlinePlayers()) {
             player.getNearbyEntities(radius, radius, radius).forEach(entity -> {
