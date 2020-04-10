f14049cda0bc94fa954c20445ecc3b027b9c2170
Leah <sc14lps@leeds.ac.uk>
2020-04-10 10:29:53 +0100

    Initial commit: Created basic Maven project containing spigot-api and bukkit dependencies

---
+++ src/main/java/com/shields/VillagerAlerts.java
@@ -0,0 +1,14 @@
+package com.shields;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VillagerAlerts extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
